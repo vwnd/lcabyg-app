57968a5f9f513f9b8d05eaf72ecf421111472eb5
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-14 10:51:44 +0100

    chore: clean up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>Home View</div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
