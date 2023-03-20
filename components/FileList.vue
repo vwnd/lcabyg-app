277d14136e983d64a3b6f34553da06f53acec2a7
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-20 21:15:01 +0100

    feat: list loaded files

---
+++ components/FileList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col space-y-6 md:max-w-md border border-current p-6 mx-auto">
+    <div for="file-input" class="block text-xs font-medium text-gray-700 mb-2">
+      Loaded Files
+    </div>
+    <div v-if="projectData.lenght > 0">
+      <div v-for="file in projectData" :key="file.fileName">
+        <div class="text-xs font-mono">
+          {{ file.fileName }}
+        </div>
+      </div>
+    </div>
+    <div v-else class="text-xs font-mono">
+      No file loaded yet.
+    </div>
+  </div>
+</template>
+
+<script setup>
+const projectData = useProjectData()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="flex  justify-center py-10  w-full">
+  <div class="flex flex-col justify-center py-10 space-y-6 w-full">
     <FileInput class="w-1/2" />
+    <FileList class="flex-1 w-full" />
   </div>
 </template>
 
