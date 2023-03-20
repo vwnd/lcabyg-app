a3034ccec3b16b4619bf415d5c947e87d3054f1a
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-20 22:11:15 +0100

    feat: request

--- components/FileInput.vue
+++ components/FileInput.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-col space-y-6 md:max-w-md border border-current p-6">
+  <div class="flex flex-col space-y-6 border border-current p-6">
     <label for="file-input" class="block text-xs font-medium text-gray-700 mb-2">Data Input</label>
     <div class="mt-2 sm:col-span-2 sm:mt-0">
-      <div ref="dragAndDropZone" class="flex max-w-lg justify-center border border-dashed border-slate-500 px-6 pt-5 pb-6">
+      <div ref="dragAndDropZone" class="flex justify-center border border-dashed border-slate-500 px-6 pt-5 pb-6">
         <div class="space-y-1 text-center">
           <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
             <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <div class="flex flex-col justify-center py-10 px-6 space-y-6 w-1/2">
       <FileInput class="w-full " />
       <FileList class=" w-full " />
-      <div class="w-full flex flex-row-reverse  max-w-md">
-        <TheButton text="Submit" />
+      <div class="w-full flex flex-row-reverse">
+        <TheButton text="Submit" @click="handleSubmit" />
       </div>
     </div>
   </div>
@@ -12,7 +12,35 @@
 
 <script setup lang="ts">
 const authToken = useAuthToken()
+const projectData = useProjectData()
 const router = useRouter()
 
 if (!authToken.value) { router.push('auth/login') }
+
+async function handleSubmit () {
+  if (!authToken.value || projectData.value.length === 0) { return }
+  let inputData: Object[] = []
+  projectData.value.forEach((d) => {
+    inputData = [...inputData, ...d.data]
+  })
+  const blob = btoa(JSON.stringify(inputData))
+
+  const { data } = await useFetch('https://api1.lcabyg.dk/v2/jobs', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${authToken.value}`
+    },
+    body: {
+      priority: 0,
+      job_target: 'lcabyg5+br23',
+      job_target_min_ver: '',
+      job_target_max_ver: '',
+      job_arguments: '',
+      extra_input: '',
+      input_blob: blob
+    }
+  })
+
+  console.log(data)
+}
 </script>
