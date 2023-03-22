df51136b56f6b360cf4c0d3bb9eb5ece21791b06
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-03-22 09:00:39 +0100

    feat: add building class

---
+++ lib/building.ts
@@ -0,0 +1,22 @@
+export class Building {
+  id?: string
+  scenario_name?: string
+  locked?: string
+  description?: {[key: string]: string}
+  building_type?: string
+  heated_floor_area?: number
+  gross_area?: number
+  integrated_garage?: number
+  external_area?: number
+  gross_area_above_ground?: number
+  person_count?: number
+  storeys_above_ground?: number
+  storeys_below_ground?: number
+  storey_height?:number
+  initial_year?: number
+  calculation_timespan?: number
+  calculation_mode?: 'BR23'
+  outside_area?: number
+  plot_area?: number
+  energy_class?: 'LowEnergy'
+}
